1af12139da1bdbe7e0c142f202d41a6bdf40b628
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-14 19:25:21 +0530

    JavaScript - include variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Code With Sahand
+
+let num = 0;
+
+const value = document.querySelector('.value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn) => {
+console.log(btn);
+})
