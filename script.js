be3456f1acec08f59ae8b420b26b103dadf70dd0
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-14 21:51:50 +0530

    JavaScript - implementation counter

--- script.js
+++ script.js
@@ -6,5 +6,16 @@ const value = document.querySelector('.value');
 const btns = document.querySelectorAll('.btn');
 
 btns.forEach((btn) => {
-console.log(btn);
+    btn.addEventListener('click', (event) => {
+        const styles = event.currentTarget.classList;
+        if (styles.contains('btn-danger')) {
+            num--;
+        } else if (styles.contains('btn-success')) {
+            num++;
+        }else{
+            num = 0;
+        }
+        value.textContent = num;
+
+    })
 })
